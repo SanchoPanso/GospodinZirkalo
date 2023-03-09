ef812b27d5007b42641e81e55fed7801fc1733fa
Viktq <vik.matveeff20011@yandex.ru>
2023-03-09 13:07:20 +0300

    Оформелние

--- Final/Counters.py
+++ Final/Counters.py
@@ -1,7 +1,8 @@
 import cv2
+import numpy as np
 
-def counter():
-    input_img = cv2.imread("Pasel.jpg")
+def counter(input_img: np.ndarray):
+
     gray = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
     retval, dst = cv2.threshold(gray, 150, 1000, cv2.THRESH_BINARY)
 
@@ -12,19 +13,16 @@ def counter():
 
     new_one = cv2.drawContours(input_img, couners_finded, -1, (255, 0, 0))
 
-    while True:
-        cv2.imshow('img', new_one)
-        if cv2.waitKey(1) == 27:
-            break
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    return new_one
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    counter()
+    start_img = cv2.imread("Pasel.jpg")
+    show_img = counter(start_img)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        cv2.imshow('img', show_img)
+        if cv2.waitKey(1) == 27:
+            break
+
